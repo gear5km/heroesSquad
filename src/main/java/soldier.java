713b615cc9bf25689ef362b5fa96f0d81c33d046
gear5km <github.com/gear5km>
2022-05-07 15:44:14 +0300

    Update soldier.java

--- src/main/java/soldier.java
+++ src/main/java/soldier.java
@@ -2,4 +2,12 @@ public class soldier {
         int health;
         int speed;
         int skill;
+
+        int strength= speed+ skill;
+
+    public int getSoldierStrength(){
+        return this.strength;
     }
+
+}
+
