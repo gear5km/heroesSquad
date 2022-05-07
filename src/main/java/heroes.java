f0ca311c51f5141bfec194afe190388639cad727
gear5km <github.com/gear5km>
2022-05-07 15:37:47 +0300

    0.351
    Added numerous features

--- src/main/java/heroes.java
+++ src/main/java/heroes.java
@@ -1,4 +1,5 @@
 import static spark.Spark.*;
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -12,6 +13,14 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
              ***********************************/
             get("/", (request, response) ->{return new ModelAndView(new HashMap(), "index.hbs");
             },new HandlebarsTemplateEngine());
+
+            get("/build_squad", (request, response) -> {
+                Map<String,Object> model=new HashMap<String, Object>();
+                String user = request.queryParams("user");
+                model.put("user", user);
+                return new ModelAndView(model, buildSquad.hbs);
+            }, new HandlebarsTemplateEngine());
+
 
         }
     }
