92782557dc23425d7a8401b6947c3dea7b1e9adf
gear5km <github.com/gear5km>
2022-05-07 15:22:08 +0300

    0.31
    Rebuilt Project with New Resources

--- src/main/java/heroes.java
+++ src/main/java/heroes.java
@@ -5,11 +5,14 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
     public class heroes {
         public static void main(String[] args){
+            staticFiles.location("/public"); // Static files
+
             /************************************
              * Routes Declared Here
              ***********************************/
-            get("/index", (request, response) ->{return new ModelAndView(new HashMap(), "index.hbs");
+            get("/", (request, response) ->{return new ModelAndView(new HashMap(), "index.hbs");
             },new HandlebarsTemplateEngine());
+
         }
     }
 
