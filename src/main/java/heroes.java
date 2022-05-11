95ebfd4187ffc51b40a193e9fc5265e51da47601
gear5km <github.com/gear5km>
2022-05-11 23:48:17 +0300

    0.381

--- src/main/java/heroes.java
+++ src/main/java/heroes.java
@@ -37,26 +37,42 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
             post("/build_squad", (request, response)-> {
                 Map<String,Object> model=new HashMap<String, Object>();
-                model.put("userSection", userSection.members);
+                model.put("userSection", userSection.members); // Gets the section list for display
 
-                String brenGunnerSelected = request.queryParams("brenSelect"); //Checks if bren gunner has been selected
-                String thompsonGunnerSelected = request.queryParams("thompsonSelect"); // Checks if Thompson gunner has been selected
-                System.out.println(brenGunnerSelected);
-                System.out.println(thompsonGunnerSelected);
+                String brenGunnerName= request.queryParams("setBrenName"); //Sets bren gunner Name
+                int brenGunnerHealth = Integer.parseInt(request.queryParams("setBrenHealth")); //Sets bren gunner health
+                int brenGunnerSkill = Integer.parseInt(request.queryParams("setBrenSkill")) ; // Sets bren gunner skill
 
-                if (brenGunnerSelected.equals("true")) {
-                    userSection.members.add(soldier.brenGunner); // Adds a Brengunner to your Section
-                    model.put("userSectionHealth", userSection.members.get(0).health);
-                    model.put("userSectionSkill", userSection.members.get(0).skill);
-                }
+                String thompsonGunnerName= request.queryParams("setThompsonName"); //Sets bren gunner Name
+                int thompsonGunnerHealth = Integer.parseInt(request.queryParams("setThompsonHealth")); //Sets bren gunner health
+                int thompsonGunnerSkill = Integer.parseInt(request.queryParams("setThompsonSkill")) ; // Sets bren gunner skill
 
-                else if (thompsonGunnerSelected.equals("true")){
-                    userSection.members.add(soldier.ThompsonSMG); // Adds a Thompson Gunner to your Section
-                    model.put("userSectionHealth", userSection.members.get(1).health);
-                    model.put("userSectionSkill", userSection.members.get(1).skill);
+                String rifleManName= request.queryParams("setrifleManName"); //Sets bren gunner Name
+                int rifleManHealth = Integer.parseInt(request.queryParams("setrifleManHealth")); //Sets bren gunner health
+                int rifleManSkill = Integer.parseInt(request.queryParams("setrifleManSkill")) ; // Sets bren gunner skill
+
+
+                userSection.members.add(new soldier(brenGunnerName,brenGunnerHealth,brenGunnerSkill)); // Adds a Brengunner to your Section
+                userSection.members.add(new soldier(thompsonGunnerName,thompsonGunnerHealth,thompsonGunnerSkill)); // Adds a Brengunner to your Section
+                userSection.members.add(new soldier(rifleManName,rifleManHealth,rifleManSkill)); // Adds a Brengunner to your Section
+
+                 for(int i=0; i>userSection.members.size(); i++){
+                     int [] soldierHealth= new int[3];
+                     int [] soldierSkill= new int[3];
+                      soldierHealth[i]= userSection.members.get(i).health;
+                      soldierSkill[i]= userSection.members.get(i).skill;
+
+                     model.put("userSectionHealth", soldierHealth);
+                     model.put("userSectionSkill", soldierSkill);
                 }
 
                 return new ModelAndView(model, "buildSquad.hbs");
+            }, new HandlebarsTemplateEngine());
+
+            post("/deploy", (request, response) -> {
+                Map<String,Object> model=new HashMap<String, Object>();
+                model.put("userSectionMembers", userSection.members);
+                return new ModelAndView(model, "deploy.hbs");
             }, new HandlebarsTemplateEngine());
 
         }
--- src/main/java/soldier.java
+++ src/main/java/soldier.java
@@ -1,15 +1,17 @@
 public class soldier {
+    String name;
     int health;
     int skill;
 
-    public soldier(int health, int skill){
+    public soldier(String name,int health, int skill){
+        this.name =name;
         this.health=health;
         this.skill = skill;
     }
 
-    static soldier brenGunner = new soldier(10,6);
-    static soldier ThompsonSMG = new soldier(6,8);
-    static soldier rifleMan = new soldier(8,7);
-    static soldier sharpShooter = new soldier(4, 9);
+    //static soldier brenGunner = new soldier(10,6);
+    //static soldier ThompsonSMG = new soldier(6,8);
+    //static soldier rifleMan = new soldier(8,7);
+    //static soldier sharpShooter = new soldier(4, 9);
 }
 
